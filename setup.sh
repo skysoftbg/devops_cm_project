73240952bec2f35a679afd41d6f75e0627cbc71d
skysoftbg <expertpcin@gmail.com>
2023-04-04 04:52:16 +0300

    Create setup.sh
    Setuo.sh will create 3 instancess on AWS and install Wordpress + LAMP with Ansible.

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install python3 python3-pip python-is-python3 git curl wget -y
+
+python3 -m pip install --upgrade pip
+
+sudo apt install awscli ansible -y
+sudo aws configure
+git clone https://github.com/skysoftbg/devops_cm_project.git
+cd devops_cm_project/
+git checkout Terraform
+chmod +x tf-ubuntu.sh 
+./tf-ubuntu.sh 
+terraform version
+ssh-keygen
+cp /home/dev/.ssh/id_rsa* .
+terraform init
+terraform fmt
+terraform validate
+sudo terraform plan -out plan
+sudo terraform apply "plan"
+git checkout Ansible
+nano inventory
+ansible-playbook wp-ubuntu.yml 
+ansible-playbook wp-ubuntu.yml -i inventory 
+ssh ubuntu@107.20.100.18
+ssh ubuntu@100.24.67.247
+ssh ubuntu@54.162.169.169
+ansible-playbook wp-ubuntu.yml -i inventory 
+history > history.txt
