c022d9a070a75aa9eb7d2c098a2ba38e898f9b4a
skysoftbg <expertpcin@gmail.com>
2023-04-04 03:37:10 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIAZDDTFASQUK5RUAI7"
-  secret_key = "D15wifeTf9tqAcD5wvLvMSvibVaAy575j9flsaZV"
-  token      = "FwoGZXIvYXdzEBUaDFuTpVNNWr++vjG5CyK1AQTq5zmdnB4kHyAUxtG0rYb0N+Gm4t8gx0gwlCcCVQjfab5SXZjqz5L+WYozEUdfAwNunXz4MsJAQOBUWuGLFkjCQKCtZY/YCxoLoA/O1nWx7Fzdw9rDMyxiEn3CzIvCojBujPRaG6gsAHMKiPTz7fWs7upapY++BMxJc4kW2mQpxN/sXmECeo5TMhfe35F3723ScEoJ/qb8e+DsvICdh8+URLo5w5tIPVFEwb8ymTDjVo6uVVQogNKsoQYyLTG3qtdhf8yRcYMhimYCOPKgJta+tSQDv3lYhpD+/KMKWRcm6Esb5ss+VRudoA=="
+  access_key = ""
+  secret_key = ""
 }
